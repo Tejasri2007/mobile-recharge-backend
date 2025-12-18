9a82ed8bdad4c848cc9166dd04ce9c0fa0a0389f
Tejasri2007 <tejasri.ss2024aids@sece.ac.in>
2025-12-18 15:47:23 +0530

    Delete backend - Final directory

--- backend - Final/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-const PORT = process.env.PORT || 5000;
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
-});
